a818b46391c9be7fbf433382dcaf06e4cb81331c
rohitsinghwho <rohiisingh8655@gmail.com>
2024-01-05 16:48:31 +0530

    configured uplods and cloudinay

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDNIARY_API_SECRET 
+});
+
+const UploadOnCloudinary=async(filepath)=>{
+    try {
+        if(!filepath)return null;
+        const result = await cloudinary.uploader.upload(filepath,{
+            resource_type:"auto",
+        });
+        console.log(`File Uploded successfully on Cloudinary : ${result.url}`);
+        return result;
+    } catch (error) {
+        fs.unlinkSync(filepath);
+        return null;
+    }
+}
+
+
+export {UploadOnCloudinary}
